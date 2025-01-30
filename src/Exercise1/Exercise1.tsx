8809345bc0906d37cf62b6b4b8efeeaf7abff985
Ernes <ernes100@hotmail.com>
2025-01-30 18:50:20 +0100

    more description exercise1

--- src/Exercise1/Exercise1.tsx
+++ src/Exercise1/Exercise1.tsx
@@ -15,8 +15,11 @@ const Exercise1: React.FC = () => {
       <h1>Exercise 1</h1>
       <h2>Counter: {count}</h2>
 
+      {/* When the + button is pressed the count number should increase by 1 */}
       <button>+</button>
+      {/* When the - button is pressed the count number should decrease by 1 */}
       <button>-</button>
+      {/* When the reset button is pressed the count number should reset to 0 */}
       <button>Reset</button>
     </div>
   );
